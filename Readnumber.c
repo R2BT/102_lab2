410a78c9b670672b37082c77890edf5f27fe14c0
R2BT <theeraphat.a@ku.th>
2021-02-08 19:47:15 +0700

    Readnumber_6320500573

---
+++ Readnumber.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    long long x,y;
+    scanf("%lld",&x);
+    y = x;
+    if(x == 0)
+        printf("Zero");
+    else
+    {
+        for(n=0;y>0;n++)
+        {
+            y/=10;
+        }
+        int a[n];
+        for(i=0;i<n;i++)
+        {
+            a[i] = x%10;
+            x/=10;
+        }
+        for(i=n;i>0;i--)
+        {
+            switch(a[i-1])
+            {
+            case 0 : printf("Zero");
+                     break;
+            case 1 : printf("One");
+                     break;
+            case 2 : printf("Two");
+                     break;
+            case 3 : printf("Three");
+                     break;
+            case 4 : printf("Four");
+                     break;
+            case 5 : printf("Five");
+                     break;
+            case 6 : printf("Six");
+                     break;
+            case 7 : printf("Seven");
+                     break;
+            case 8 : printf("Eight");
+                     break;
+            case 9 : printf("Nine");
+                     break;
+            }
+            if(i!=1)
+                printf("-");
+        }
+    }
+
+    return 0;
+}
